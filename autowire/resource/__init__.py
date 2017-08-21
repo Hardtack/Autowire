30d88f1365a5eb437e1d3c19b78e435cd8bee923
Choi Geonu <6566gun@gmail.com>
2017-08-21 13:39:53 +0900

    Move Resource class to plain.py

--- autowire/resource/__init__.py
+++ autowire/resource/__init__.py
@@ -5,33 +5,9 @@ autowire.resource
 Resource implementations.
 
 """
-from autowire.base import BaseResource
-from autowire.impl.types import Implementable, Implementation
-
-from .utils import create_default_impl
+from .plain import Resource
 
 
-class Resource(BaseResource, Implementable):
-    """
-    Resource class of some functionalities like default implementation.
-
-    """
-
-    def __init__(self, name, namespace):
-        super().__init__(name, namespace)
-        self.default_implementation = create_default_impl(name, namespace)
-
-    @property
-    def default_implementation(self):
-        return self._default_implementation
-
-    @default_implementation.setter
-    def default_implementation(self, default_implementation):
-        self._default_implementation = default_implementation
-
-    def implement(self, implementation: Implementation):
-        """Set implementation of implementable."""
-        self.default_implementation = implementation
-
-
-__all__ = ['Resource']
+__all__ = [
+    'Resource',
+]
