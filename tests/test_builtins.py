c683ba5cc91f0186d3802c75eb2a03008738e84f
Choi Geonu <6566gun@gmail.com>
2017-09-06 17:02:55 +0900

    Fix builtin context resource

---
+++ tests/test_builtins.py
@@ -0,0 +1,15 @@
+from autowire import Context, impl, resource
+from autowire.builtins import context as context_resource
+
+
+def test_context():
+    @resource.create
+    @impl.autowired('context', context_resource)
+    @impl.plain
+    def get_context(context):
+        return context
+
+    context = Context()
+
+    with context.resolve(get_context) as resolved:
+        assert context == resolved
