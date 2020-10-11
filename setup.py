7772353375ad32d278d36035b230089ff145318c
Choi Geonu <6566gun@gmail.com>
2020-10-11 19:25:19 +0900

    Bump version up to 1.0.0

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(
     name="Autowire",
     packages=setuptools.find_packages(exclude=["tests"]),
-    version="1.0.0-dev",
+    version="1.0.0",
     description="Simple dependency injection.",
-    author="Geonu Choi",
+    author="Choi Geonu",
     author_email="6566gun@gmail.com",
     url="https://github.com/hardtack/autowire",
     license="MIT LICENSE",
